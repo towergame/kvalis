5d69679c1e1acf21382090ca9ba673e9e6b43935
tower <towergame.epic@gmail.com>
2025-01-06 03:42:46 +0200

    Comments

--- Kvalis/Context/Entities/Ballot.cs
+++ Kvalis/Context/Entities/Ballot.cs
@@ -7,9 +7,9 @@ public class Ballot
     [Key]
     public Guid Id { get; set; }
     
-    public byte[]? PublicKey { get; set; }
+    public byte[]? PublicKey { get; set; } // If this is not null, the ballot has been received
     
-    public bool Submitted { get; set; }
+    public bool Submitted { get; set; } // If this is true, the ballot has been submitted
 
     public virtual Election Election { get; set; }
 }
